23870f289cc2dbfdac4a1ea08ea739c16caedd1b
Kyle Harbaugh <harbaugh6@gmail.com>
2024-06-20 16:49:38 -0700

    Uploading tests to repo

---
+++ playwrite/tests/homepageFixture.spec.ts
@@ -0,0 +1,30 @@
+import { test as base, expect } from '@playwright/test';
+import { Homepage } from '../fixtures/homepage';
+
+// Extend basic test by providing a Homepage fixture.
+const test = base.extend<{ homepage: Homepage }>({
+    homepage: async ({page}, use) => {
+        const homepage = new Homepage(page);
+        await page.goto('/');
+        await expect(page).toHaveTitle(/Capitol Alumni Network/);
+        await use(homepage);
+    },
+});
+
+// test.beforeEach(async ({page }) => {
+//     await page.goto('/');
+// })
+
+test('should log into the CAN site', async ({page, homepage}) => {
+    await homepage.signIn();
+    await page.getByPlaceholder('Email address').click();
+    await page.getByPlaceholder('Email address').fill('string');
+    await page.getByPlaceholder('Password').fill('string');
+    await page.getByRole('button', { name: 'Sign in with LeagueApps' }).click();
+})
+
+test('should go to the Softball page', async ({page, homepage}) => {
+    await homepage.goToSoftball();
+    await page.getByRole('link', { name: 'Teams, Schedule and Standings' }).click();
+    await page.getByRole('link', { name: 'Standings' }).click();
+})
