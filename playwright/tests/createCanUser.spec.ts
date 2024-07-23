4cd63c929d9c8aa99643c0c34d1dcdaf18f30726
Kyle Harbaugh <harbaugh6@gmail.com>
2024-07-23 13:45:51 -0700

    Completing quick sign up test

--- playwright/tests/createCanUser.spec.ts
+++ playwright/tests/createCanUser.spec.ts
@@ -1,15 +1,14 @@
 import {test, expect} from '@playwright/test';
 import { Homepage } from '../fixtures/homepage';
 import {faker} from '@faker-js/faker/locale/en_US';
-import {UserCreator} from '../fixtures/userCreator';
 
 let homepage;
 const baseURL = 'http://capalum.org';
 // Creating some test data here in the test itself as an example of what you can do.
 // Whether you -should- do it this way is up for debate.
 const firstName = faker.person.firstName();
-const lastName = faker.person.lastName();
-const email = faker.internet.email(firstName, lastName + "+Test1234");
+const lastName = (faker.person.lastName() + '+Test1234');
+const email = faker.internet.email({firstName, lastName})
 
 
 test.describe('Can create a new account', () => {
@@ -21,12 +20,15 @@ test.describe('Can create a new account', () => {
         await page.goto(baseURL);
         await expect(page).toHaveTitle("Capitol Alumni Network");
         await homepage.signUpLink.click();
-        await expect(page).toHaveTitle("LeagueApps Login :: Create Account")
+        await expect(page).toHaveTitle("LeagueApps Login :: Create Account");
+        await expect(page.getByRole("textbox", {name: "First Name"})).toBeVisible();
         await page.getByRole("textbox", {name: "First Name"}).fill(firstName);
         await page.getByRole("textbox", {name: "Last Name"}).fill(lastName);
         await page.getByRole("textbox", {name: "Email Address"}).fill(email);
-        await page.getByRole("textbox", {name: "Password"}).fill(faker.internet.password())
-        // At this point the user would click "Sign Up", however LeagueApps has implemented a
-        // pretty stringent anti-automation policy that has thwarted my attempt to complete this test.
+        // Hardcoding the password for ease of use and to meet the requirements.
+        await page.getByRole("textbox", {name: "Password"}).click();
+        await page.getByRole("textbox", {name: "Password"}).pressSequentially('Test1234$')
+        await page.getByRole("button", {name: "Create a LeagueApps account"}).click();
+        await expect(page.getByRole('heading', { name: 'Success!' })).toBeVisible();
     })
 })
