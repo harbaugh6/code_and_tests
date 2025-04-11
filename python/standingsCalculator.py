d2562a278c41e962312cb4fd191f4c20a4c7dd2b
Kyle Harbaugh <harbaugh6@gmail.com>
2025-04-11 14:50:42 -0700

    simplified function

--- python/standingsCalculator.py
+++ python/standingsCalculator.py
@@ -14,14 +14,22 @@ class StandingsCalculator(object):
     def winningPercentage(self, wins=0, losses=0):
         self.wins = wins
         self.losses = losses
-        return (round((wins / (wins + losses)), 3))
+        totalGames = wins + losses
+        if totalGames == 0:
+            return 0.0
+        winningPercentage = round((wins / totalGames), 3)
+        return winningPercentage
     
     def divisionWinningPercentage(self, divisionWins=0, divisionLosses=0):
         self.divisionWins = divisionWins
         self.divisionLosses = divisionLosses
-        return (round((divisionWins / (divisionWins + divisionLosses)), 3))
-    
+        totalDivisionGames = divisionWins + divisionLosses
+        if totalDivisionGames == 0:
+            return 0.0
+        winningPercentage = round((divisionWins / totalDivisionGames), 3)
+        return winningPercentage
+        
 standingsCalculator = StandingsCalculator("Test")
     
 print(standingsCalculator.winningPercentage(14, 3))
-print(standingsCalculator.divisionWinningPercentage(5, 1))
+print(standingsCalculator.divisionWinningPercentage(5, 3))
